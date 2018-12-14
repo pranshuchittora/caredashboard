9c6ad6927703fcd3f9a536896fdf52f1a7f3d81a
Pranshu Chittora <pranshuchittora17@gmail.com>
2018-12-15 02:08:22 +0530

    Fixs LS object comparison (Lodash depricated)

--- src/util/UpdateLocalStorage.js
+++ src/util/UpdateLocalStorage.js
@@ -12,11 +12,31 @@ export default function(camp, dep, lab, url) {
   let CurrentLS = [];
   if (FetchLS.default()) CurrentLS = FetchLS.default();
 
-  const timestamp = new Date()
-  let newData = { campus: camp, department: dep, label: lab, link: url,time:timestamp };
+  const timestamp = new Date();
+  let newData = {
+    campus: camp,
+    department: dep,
+    label: lab,
+    link: url,
+    time: timestamp
+  };
+
+  //Remove Unwanted
+  if (CurrentLS.constructor == Array) {
+    for (let i = 0; i < CurrentLS.length; i++) {
+      let dataMatchBool =
+        CurrentLS[i].campus === newData.campus &&
+        CurrentLS[i].department === newData.department &&
+        CurrentLS[i].label === newData.label &&
+        CurrentLS[i].link === newData.link;
+
+      if (dataMatchBool) CurrentLS.splice(i, 1);
+    }
+  }
+
   CurrentLS.unshift(newData);
   localStorage.clear();
-  const NewLS = uniqWith(CurrentLS, isEqual);
+  const NewLS = CurrentLS;
 
   localStorage.setItem("care-history", JSON.stringify(NewLS.slice(0, 5)));
 }
