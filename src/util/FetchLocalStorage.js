cfe70f2b73e263714e2d0863e89a83660aef8f7d
Pranshu Chittora <pranshuchittora17@gmail.com>
2018-12-16 12:27:30 +0530

    Refactoring to CRA 2.0

--- src/util/FetchLocalStorage.js
+++
@@ -1,5 +0,0 @@
-const LS_KEY = "care-history";
-
-export default function() {
- return JSON.parse(localStorage.getItem(LS_KEY))
-}
--- src/util/timeStamp.js
+++
@@ -1,5 +0,0 @@
-const moment = require('moment')
-
-let data = new Date()
-console.log(data)
-console.log(moment(data).fromNow());
